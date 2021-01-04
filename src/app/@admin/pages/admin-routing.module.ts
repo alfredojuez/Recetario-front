d0f38089ff6e1de8c5dd3afd15cb8b729452aac5
Alfredo Juez <Alfredojuez@hotmail.com>
2021-01-04 16:28:08 +0100

    Version inicial publico y privado

---
+++ src/app/@admin/pages/admin-routing.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { AdminComponent } from './admin.component';
+
+const routes: Routes = [
+  {
+    path: 'admin',
+    component: AdminComponent,
+    children: [
+      {
+        path: 'dashboard',
+        loadChildren: () =>
+          import('./dashboard/dashboard.module').then((m) => m.DashboardModule),
+      },
+      {
+        path: 'usuarios',
+        loadChildren: () =>
+          import('./usuarios/usuarios.module').then((m) => m.UsuariosModule),
+      },
+    ],
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class AdminRoutingModule {}
---
+++ src/app/@public/pages/public-routing.module.ts
@@ -0,0 +1,41 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { PublicComponent } from './public.component';
+
+
+const routes: Routes = [
+  {
+    path: '',
+    component: PublicComponent,
+    children: [
+      {
+        path: 'pagina404',
+        loadChildren: () =>
+          import('./pagina404/pagina404.module').then(
+            (m) => m.Pagina404Module
+          ),  
+      },
+      {
+        path: 'contacto',
+        loadChildren: () =>
+          import('./contacto/contacto.module').then(
+            (m) => m.ContactoModule
+          ),
+      },
+      {
+        path: 'inicio',
+        
+        loadChildren: () =>
+          import('./inicio/inicio.module').then(
+            (m) => m.InicioModule
+          ),
+      }
+    ]
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class PublicRoutingModule { }
