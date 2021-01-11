0a1640d7b74831f7656854f2bf7776845339b32a
Alfredo Juez <Alfredojuez@hotmail.com>
2021-01-11 12:10:01 +0100

    mostramos el menu o lo ocultamos

---
+++ src/app/@admin/core/components/barralateral/barralateral.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-barralateral',
+  templateUrl: './barralateral.component.html',
+  styleUrls: ['./barralateral.component.scss']
+})
+export class BarralateralComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
