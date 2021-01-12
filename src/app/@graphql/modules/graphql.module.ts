0348a70967c9895c12f52166d45fc3485de6f185
Alfredo Juez <Alfredojuez@hotmail.com>
2021-01-12 12:37:31 +0100

    Configuracion de acceso a graphQL
    Instalamos las librerias de uso y deteccion de errores
    Configuramos la ruta de acceso
    y parametrizamos la URL del graphiQL para acceder a el

---
+++ src/app/@graphql/modules/graphql.module.ts
@@ -0,0 +1,38 @@
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { Apollo, ApolloModule } from 'apollo-angular';
+import { HttpLink, HttpLinkModule } from 'apollo-angular-link-http'
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { ApolloLink } from 'apollo-link';
+import { onError } from 'apollo-link-error';
+
+@NgModule({
+  imports: [
+    HttpClientModule,
+    ApolloModule,
+    HttpLinkModule
+  ]
+})
+export class GraphqlModule { 
+  constructor(apollo: Apollo, httpLink: HttpLink){
+      //Para capturar los errores de consulta y/o de red
+      
+     
+      const errorLink = onError(({graphQLErrors, networkError}) =>{
+        if(graphQLErrors)
+        {
+          console.log(`Errores de GraphQL: ${graphQLErrors}`);
+        }
+
+        if(networkError)
+        {
+          console.log(`Errores de RED: ${networkError}`)
+        }
+
+        //URL del playground de GraphQL
+        const uri = 'http://localhost:2004/graphql';
+        const link = ApolloLink.from ([errorLink, httpLink.create({uri})]);
+        apollo.create({link, cache: new InMemoryCache});
+      });
+  };
+}
