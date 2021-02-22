b0f592495525dac1473aa863d1e4870ecc835647
Alfredo Juez <Alfredojuez@hotmail.com>
2021-02-22 13:55:34 +0100

    correcciones tslint

--- src/app/@public/pages/public-routing.module.ts
+++ src/app/@public/pages/public-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { ChangePasswordModule } from './forms/change-password/change-password.module';
 import { PublicComponent } from './public.component';
 
 
